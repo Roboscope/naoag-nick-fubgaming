c52774a55ec0dc06c1412ea4907d1e7f44342afc
Nick <nick.ligmann1503@gmail.com>
2017-07-14 15:32:30 +0200

    commit message

--- app/src/main/java/com/nickljackson/fubgaming/MainActivity.java
+++ app/src/main/java/com/nickljackson/fubgaming/MainActivity.java
@@ -1,13 +1,87 @@
 package com.nickljackson.fubgaming;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
+    private Toolbar mToolbar;
+
+    private FragmentTransaction fragmentTransaction;
+    NavigationView mNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // setzt das View für die Actionbar
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+        //setzt den View für den NavDrawer & den button oben links
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        //setzt den titel
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true );
+        getSupportActionBar().setTitle("Members");
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new MemberFragment());
+        fragmentTransaction.commit();
+        mNavView = (NavigationView) findViewById(R.id.nav_view);
+        mNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_members:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new MemberFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Members");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_temine:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new TerminFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Termine");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_account:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new SettingsFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Account Settings");
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_logout:
+                        break;
+                }
+                return true;
+            }
+        }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/nickljackson/fubgaming/UserProfile.java
@@ -0,0 +1,18 @@
+package com.nickljackson.fubgaming;
+
+/**
+ * Created by Nick on 03.07.2017.
+ */
+
+public class UserProfile {
+    String userName;
+    int steamID;
+    String displayName;
+    int status;
+    int game;
+
+    public UserProfile(int pSteamID){
+
+    }
+
+}
